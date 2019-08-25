5ca42c1bf2161a2535ec905328d49dc87972898e
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-25 07:30:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,12 @@
 class Song
-  attr_accessor :title
+  attr_accessor :name, :artist
 
-  def initialize
+   def initialize(name, artist=nil)
+    @name = name
+    @artist = artist
   end
-end
+
+   def artist_name
+    self.artist ? self.artist.name : nil
+  end
+end 
